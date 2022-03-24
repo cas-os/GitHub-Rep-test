85bd74f591c8cb4fc079c57491677d70ae77fe8f
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-03-24 16:51:58 +0100

    Initial Commit

---
+++ GitHub Rep test/GitHub_Rep_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_Rep_testApp.swift
+//  GitHub Rep test
+//
+//  Created by HAL-9001 on 24/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_Rep_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
